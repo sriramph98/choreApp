a833da5da7383d2797aee4997c05a4f5b2408fc0
Sriram P H <sriramph9812@gmail.com>
2024-12-22 09:17:12 -0500

    Initial Commit

---
+++ Chore/ChoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoreApp.swift
+//  Chore
+//
+//  Created by Sriram P H on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ChoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChoreTests/ChoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChoreTests.swift
+//  ChoreTests
+//
+//  Created by Sriram P H on 12/22/24.
+//
+
+import Testing
+@testable import Chore
+
+struct ChoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
